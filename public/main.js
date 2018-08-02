424a98bd8a4a26f68929ac52538b9889e608cb45
Michael Solone <michaelsolone@gmail.com>
2018-08-02 10:02:47 -0400

    The button randomly changing the background color

--- public/main.js
+++ public/main.js
@@ -5,14 +5,18 @@
 // document.addEventListener('DOMContentLoaded', main)
 
 const colorNumbers = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-const colorCodeArray = []
+const colorCodeArray = ['#']
+
+
 
 const randomColor = () => {
+  const colorCodeArray = ['#']
   console.log('this works')
   for (i = 0; i < 6; i++) {
     const randomIndex = colorNumbers[Math.floor(Math.random() *15)]
     colorCodeArray.push(randomIndex)
     colorCode = colorCodeArray.join('')
+    document.body.style.background = colorCode
 
   }
 }
