7ba49dcab5dc4a0eb0a4411cccf0f472ff1475b5
Michael Solone <michaelsolone@gmail.com>
2018-08-02 09:33:12 -0400

    Can randomly generate color code

--- public/main.js
+++ public/main.js
@@ -1,5 +1,20 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
+
+// document.addEventListener('DOMContentLoaded', main)
+
+const colorNumbers = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+const colorCodeArray = []
+
+const randomColor = () => {
+  console.log('this works')
+  for (i = 0; i < 6; i++) {
+    const randomIndex = colorNumbers[Math.floor(Math.random() *15)]
+    colorCodeArray.push(randomIndex)
+    colorCode = colorCodeArray.join('')
+
+  }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.change-color-button').addEventListener('click', randomColor)
